7bc3772414f9c315f225bb771e8cd98b4ed0a662
Aboutuspage <peteryares276@gmail.com>
2025-09-11 16:12:16 +0800

    push update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import Hero from "./sections/Hero";
+import WhoWeAre from "./sections/WhoWeAre";
 
 export default function Page() {
   return (
     <Box component={"main"} sx={{ overflowX: "hidden" }}>
       <Hero />
+      <WhoWeAre />
     </Box>
   );
 }
