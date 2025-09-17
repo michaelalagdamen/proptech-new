fd7c2c9b1943954affb941a11b2cecf349e1edeb
Aboutuspage <peteryares276@gmail.com>
2025-09-17 14:30:50 +0800

    push updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,7 @@
-import Footer from "@/component/sections/Footer";
-import Hero from "@/component/sections/Hero";
-<<<<<<< HEAD
-=======
-import Navs from "@/component/sections/Navs";
-import WhoWeAre from "@/component/sections/WhoWeAre";
-import WhyTrustUs from "@/component/sections/WhyTrustUs";
->>>>>>> 485bb26371d23216bfe89551d8671cd9ee150903
 import { Box } from "@mui/material";
 import AboutHero from "@/component/sections/AboutHero";
 import MissionVision from "@/component/sections/MissionVision";
+import Footer from "@/component/sections/Footer";
 
 export default function About() {
   return (
@@ -20,16 +13,9 @@ export default function About() {
         height: "100%",
       }}
     >
-<<<<<<< HEAD
       <AboutHero />
       <MissionVision />
-=======
-      <Hero />
-      <Navs />
-      <WhoWeAre />
-      <WhyTrustUs />
       <Footer />
->>>>>>> 485bb26371d23216bfe89551d8671cd9ee150903
     </Box>
   );
 }
