2bcc7bfe11673e1c9f18f228122e8a90c20e0687
Aboutuspage <peteryares276@gmail.com>
2025-09-23 11:38:45 +0800

    pull updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
 import FeaturedCompany from "@/component/sections/FeaturedCompany";
 import FeaturedProducts from "@/component/sections/FeaturedProducts";
-<<<<<<< HEAD
-import Footer from "@/component/sections/Footer";
-=======
-
->>>>>>> 58097d902f4b5fa02e96bf0bb338e5f80cc326f5
 import WhoWeAre from "@/component/sections/WhoWeAre";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <Box
-      sx={{
-        overflowX: "hidden",
-        backgroundColor: "#FFFFFF",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-=======
     <>
->>>>>>> 58097d902f4b5fa02e96bf0bb338e5f80cc326f5
       <WhoWeAre />
       <FeaturedCompany />
       <FeaturedProducts />
